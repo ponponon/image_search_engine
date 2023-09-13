ba256df63557b9b03947b95798da23647e698c6a
jiancheng <1729303158@qq.com>
2023-09-13 17:19:06 +0800

    项目描述

--- api.py
+++ api.py
@@ -8,8 +8,23 @@ from apps.meta import meta
 
 version = "2023.09.07.1"
 
+description = """
+这个系统的逻辑是，把图片转成向量，512 维度的向量，512 个  float，一个 float 4字节
+
+所以一个图片生成后的向量是 2048字节
+
+存储在向量数据库里面，向量数据库是内存数据库，等于一个图片要占用 2048 字节
+
+----
+
+
+生成向量的效率：单核每秒生成5个向量
+检索向量的效率：取决于母本量
+"""
+
 app = FastAPI(title='「以图搜图」接口', debug=False,
-              docs_url=None, redoc_url=None, version=version)
+              docs_url=None, redoc_url=None, version=version, description=description)
+
 
 app.add_middleware(
     CORSMiddleware,
